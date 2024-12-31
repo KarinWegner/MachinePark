ce9b0d075eab8837b4d2f793914cf7aab9314206
KarinWegner <karinwegner00@gmail.com>
2024-12-31 09:13:52 +0100

    Added details page

---
+++ Components/Pages/MachineDetails.razor.cs
@@ -0,0 +1,25 @@
+﻿using MachinePark.Entities;
+using MachinePark.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace MachinePark.Components.Pages
+{
+    public partial class MachineDetails
+    {
+        [Parameter]
+        public int MachineID { get; set; }
+
+        private Machine Machine { get; set; } = new Machine();
+
+        protected override void OnInitialized()
+        {
+            Machine = MachineStorageService.MachineGarage.Single(m => m.Id == MachineID);
+        }
+        private void ChangeActiveStatus()
+        {
+            Machine.IsRunning = !Machine.IsRunning;
+        }
+
+
+    }
+}
